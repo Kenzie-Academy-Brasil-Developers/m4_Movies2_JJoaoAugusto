e7c95950fea7f14adef1627dfe0935d4dc2e3ce8
João Augusto <jaugustovpires@gmail.com>
2023-08-28 10:21:30 -0300

    fix: pagination

--- src/middlewares/pagination.middlewares.ts
+++ src/middlewares/pagination.middlewares.ts
@@ -6,16 +6,16 @@ export const pagination = (
   res: Response,
   next: NextFunction
 ): void => {
-  const queryPage: number = Number(req.query.page); //offset
-  const queryPerPage: number = Number(req.query.perpage); //limit
+  const queryPage: number = Number(req.query.page);
+  const queryPerPage: number = Number(req.query.perPage);
 
   const page: number = queryPage && queryPage > 1 ? queryPage : 1;
   const perPage: number =
     queryPerPage && queryPerPage <= 5 && queryPerPage > 0 ? queryPerPage : 5;
 
-  const baseUrl: string = "http://localgost:3000/products";
-  const prevPage: string = `${baseUrl}?page=${page - 1}&perpage=${perPage}`;
-  const nextPage: string = `${baseUrl}?page=${page + 1}&perpage=${perPage}`;
+  const baseUrl: string = "http://localgost:3000/movies";
+  const prevPage: string = `${baseUrl}?page=${page - 1}&perPage=${perPage}`;
+  const nextPage: string = `${baseUrl}?page=${page + 1}&perPage=${perPage}`;
 
   const queryOrder: any = req.query.order;
   const querySort: any = req.query.sort;
